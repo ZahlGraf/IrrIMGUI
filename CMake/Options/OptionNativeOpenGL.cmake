74432aaffe84b4da55c93d4d02b6f17c48e56424
Andre Netzeband <andre@netzeband.eu>
2015-09-22 00:06:05 +0200

    Added new option for risky but very fast OpenGL handle translation.

--- CMake/Options/OptionNativeOpenGL.cmake
+++ CMake/Options/OptionNativeOpenGL.cmake
@@ -1,11 +1,22 @@
 SET (IRRIMGUI_NATIVE_OPENGL OFF CACHE BOOL "Enables the native OpenGL Driver for IMGUI (ATTENTION: This is just a fallback solution!).")
 
-if (IRRIMGUI_STATIC_LIBRARY)
+if (IRRIMGUI_NATIVE_OPENGL)
 	message(STATUS "Use native OpenGL render driver (Fallback solution, do not use it for productive work!)...")
 	ADD_DEFINITIONS(	
 	-D_IRRIMGUI_NATIVE_OPENGL_
 	)
+	SET (IRRIMGUI_FAST_OPENGL_TEXTURE_CREATION ON CACHE BOOL "Enables the fast OpenGL texture creation to translate a ITexture object to an OpenGL handle instead of creating a copy. This is a very instable hack. Disable this, when the example 7 (RenderWindow) fails.")	
+	INCLUDE(DependencyOpenGL)	
 else ()
 	message(STATUS "Use Irrlicht based render driver...")
-	INCLUDE(DependencyOpenGL)	
 endif ()
+
+if (IRRIMGUI_FAST_OPENGL_TEXTURE_CREATION)
+	message(STATUS "Use fast OpenGL texture ID creation (fast but risky!)...")
+	ADD_DEFINITIONS(	
+	-D_IRRIMGUI_FAST_OPENGL_TEXTURE_HANDLE_
+	)
+else ()
+	message(STATUS "Creaty texture copy for generating OpenGL texture IDs...")
+endif ()
+
