100e1af5b4ccafb36dedf98f8ce44154ad926845
Andre Netzeband <andre@netzeband.eu>
2015-09-25 19:24:12 +0200

    Do not search for OpenGL Dependency if native OpenGL support has been disabled.

--- CMake/Dependencies/DependencyOpenGL.cmake
+++ CMake/Dependencies/DependencyOpenGL.cmake
@@ -1,18 +1,20 @@
-message(STATUS "Looking for OpenGL header and library...")
+if (IRRIMGUI_NATIVE_OPENGL)
+	message(STATUS "Looking for OpenGL header and library...")
 
-find_package(OpenGL)
+	find_package(OpenGL)
 
-if (OPENGL_FOUND)
-    MESSAGE(STATUS "OpenGL Correctly Found:")
-	MESSAGE(STATUS " * Include-Dir: ${OPENGL_INCLUDE_DIR}")
-	MESSAGE(STATUS " * Libraries: ${OPENGL_LIBRARIES}")	
-    INCLUDE_DIRECTORIES(${OPENGL_INCLUDE_DIR})
-	SET(IRRIMGUI_DEPENDENCY_LIBRARIES
-	${IRRIMGUI_DEPENDENCY_LIBRARIES}
-	${OPENGL_LIBRARIES}
-	)
+	if (OPENGL_FOUND)
+		MESSAGE(STATUS "OpenGL Correctly Found:")
+		MESSAGE(STATUS " * Include-Dir: ${OPENGL_INCLUDE_DIR}")
+		MESSAGE(STATUS " * Libraries: ${OPENGL_LIBRARIES}")	
+		INCLUDE_DIRECTORIES(${OPENGL_INCLUDE_DIR})
+		SET(IRRIMGUI_DEPENDENCY_LIBRARIES
+		${IRRIMGUI_DEPENDENCY_LIBRARIES}
+		${OPENGL_LIBRARIES}
+		)
 	
-else ()
-    MESSAGE(STATUS "OpenGL environment missing")
+	else ()
+		MESSAGE(STATUS "OpenGL environment missing")
 	
+	endif ()
 endif ()
