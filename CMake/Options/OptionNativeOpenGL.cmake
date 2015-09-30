6ef1d66d9204081d2843051813f488b62f36857a
Andre Netzeband <andre@netzeband.eu>
2015-09-30 13:56:06 +0200

    Print Warning when compiling with native OpenGL renderer to highlight, that this is just a fall-back solution.

--- CMake/Options/OptionNativeOpenGL.cmake
+++ CMake/Options/OptionNativeOpenGL.cmake
@@ -25,7 +25,7 @@
 SET (IRRIMGUI_NATIVE_OPENGL OFF CACHE BOOL "Enables the native OpenGL Driver for IMGUI (ATTENTION: This is just a fallback solution!).")
 
 if (IRRIMGUI_NATIVE_OPENGL)
-	message(STATUS "Use native OpenGL render driver (Fallback solution, do not use it for productive work!)...")
+	message(WARNING "Use native OpenGL render driver (Fallback solution, do not use it for productive work!)...")
 	ADD_DEFINITIONS(	
 	-D_IRRIMGUI_NATIVE_OPENGL_
 	)
