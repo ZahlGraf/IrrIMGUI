dd75af02c69b3590f7bd1a7c921642e98c194728
Andre Netzeband <andre@netzeband.eu>
2015-10-10 23:54:44 +0200

    Added new macros for Range and Tolerance testing.

--- includes/IrrIMGUI/UnitTest/UnitTest.h
+++ includes/IrrIMGUI/UnitTest/UnitTest.h
@@ -65,6 +65,18 @@
 /// @param Argument2 Is the second argument to check.
 #define CHECK_NOT_EQUAL(Argument1, Argument2)
 
+/// @brief Checks if two values are equal with considering a tolerance.
+/// @param ReferenceValue Is the reference value for that check.
+/// @param Tolerance      Is the tolerance, where the tested value must be inside.
+/// @param TestValue      Is the value to test.
+#define CHECK_EQUAL_TOLERANCE(ReferenceValue, Tolerance, TestValue)
+
+/// @brief Check a value in in range between minimum and maximum.
+/// @param Minimum Is the lower border.
+/// @param Maximum Is the higher border.
+/// @param TestValue      Is the value to test.
+#define CHECK_IN_RANGE(Minimum, Maximum, TestValue)
+
 /// @brief Creates a mock entry into the mock database for the current function/method.
 #define MOCK_FUNC()
 
@@ -79,9 +91,24 @@
 #define CHECK_NOT_EQUAL_LOCATION(Argument1, Argument2, file, line) \
 {                                      \
   CHECK((Argument1) != (Argument2));   \
-  if ((Argument1) != (Argument1)) {UtestShell::getCurrent()->print("WARNING:\n\tThe \"Actual Parameter\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
-  if ((Argument2) != (Argument2)) {UtestShell::getCurrent()->print("WARNING:\n\tThe \"Actual Parameter\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
+  if ((Argument1) != (Argument1)) {UtestShell::getCurrent()->print("WARNING:\n\tThe \"Argument1\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
+  if ((Argument2) != (Argument2)) {UtestShell::getCurrent()->print("WARNING:\n\tThe \"Argument2\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
 }                                      \
+
+#define CHECK_EQUAL_TOLERANCE(ReferenceValue, Tolerance, TestValue) \
+  CHECK_IN_RANGE((ReferenceValue)-(Tolerance), (ReferenceValue)+(Tolerance), (TestValue))
+
+#define CHECK_IN_RANGE(Minimum, Maximum, TestValue) \
+  CHECK_IN_RANGE_LOCATION(Minimum, Maximum, TestValue, __FILE__, __LINE__)
+
+#define CHECK_IN_RANGE_LOCATION(Minimum, Maximum, TestValue, file, line) \
+{                                                   \
+  CHECK((TestValue) >= (Minimum));                  \
+  CHECK((TestValue) <= (Maximum));                  \
+  if ((TestValue) != (TestValue)) {UtestShell::getCurrent()->print("WARNING:\n\tThe \"TestValue\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
+  if ((Minimum)   != (Minimum))   {UtestShell::getCurrent()->print("WARNING:\n\tThe \"Minimum\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
+  if ((Maximum)   != (Maximum))   {UtestShell::getCurrent()->print("WARNING:\n\tThe \"Minimum\" parameter is evaluated multiple times resulting in different values.\n\tThus the value in the error message is probably incorrect.", file, line);}   \
+}                                                   \
 
 #define MOCK_FUNC()         mock().actualCall(__FUNCTION__)
 #define MOCK_ARG(Argument)  withParameter(#Argument, Argument)
