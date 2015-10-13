a4b192a594c303d28bebe78b71fe7525377355bf
Andre Netzeband <andre@netzeband.eu>
2015-10-13 23:02:37 +0200

    Added further unit tests for the IrrIMGUI handle.

--- unittests/TestIrrIMGUIHandle.cpp
+++ unittests/TestIrrIMGUIHandle.cpp
@@ -33,6 +33,7 @@
 #include <IrrIMGUI/IIMGUIHandle.h>
 #include <IrrIMGUI/IrrIMGUI.h>
 #include <IrrIMGUI/Inject/IrrIMGUIInject.h>
+#include <IrrIMGUI/IrrIMGUIConstants.h>
 
 using namespace IrrIMGUI;
 
@@ -47,17 +48,13 @@ TEST_GROUP(TestIMGUIHandle)
   }
 };
 
-TEST(TestIMGUIHandle, createAndDestroyInstance)
+TEST(TestIMGUIHandle, checkIfHandleGrabsAndDropsIrrlicht)
 {
-  Inject::setIMGUIFactory();
-
   irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
-  CIMGUIEventStorage EventStorage;
-  SIMGUISettings Settings;
 
   irr::u32 const IrrDeviceRefCount = pDevice->getReferenceCount();
 
-  IIMGUIHandle * const pGUI = createIMGUI(pDevice, &EventStorage, &Settings);
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
 
   CHECK(pDevice->getReferenceCount() > IrrDeviceRefCount);
 
@@ -70,6 +67,222 @@ TEST(TestIMGUIHandle, createAndDestroyInstance)
   return;
 }
 
+TEST(TestIMGUIHandle, checkIfHandleSetupsIMGUIMouse)
+{
+  ImGuiIO &rIMGUI = ImGui::GetIO();
 
+  for (int i = 0; i < Const::NumberOfMouseButtons; i++)
+  {
+    rIMGUI.MouseClicked[i]          = true;
+    rIMGUI.MouseDoubleClicked[i]    = true;
+    rIMGUI.MouseDown[i]             = true;
+    rIMGUI.MouseClickedTime[i]      = 42.0f;
+    rIMGUI.MouseDownDurationPrev[i] = 52.0f;
+    rIMGUI.MouseDownDuration[i]     = 62.0f;
+  }
 
+  rIMGUI.MouseWheel = 2.0f;
 
+  irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
+
+  for (int i = 0; i < Const::NumberOfMouseButtons; i++)
+  {
+    CHECK_EQUAL(false,    rIMGUI.MouseClicked[i]);
+    CHECK_EQUAL(false,    rIMGUI.MouseDoubleClicked[i]);
+    CHECK_EQUAL(false,    rIMGUI.MouseDown[i]);
+    CHECK_EQUAL(-FLT_MAX, rIMGUI.MouseClickedTime[i]);
+    CHECK_EQUAL(-1.0f,    rIMGUI.MouseDownDurationPrev[i]);
+    CHECK_EQUAL(-1.0f,    rIMGUI.MouseDownDuration[i]);
+  }
+
+  CHECK_EQUAL(0.0f, rIMGUI.MouseWheel);
+
+  pGUI->drop();
+  pDevice->drop();
+
+  return;
+}
+
+TEST(TestIMGUIHandle, checkIfHandleSetupsFonts)
+{
+  ImGuiIO &rIMGUI = ImGui::GetIO();
+
+  rIMGUI.Fonts->Clear();
+  rIMGUI.Fonts->TexID = nullptr;
+
+  irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
+
+  CHECK_NOT_EQUAL(nullptr, rIMGUI.Fonts->TexID);
+  CHECK_EQUAL(1,           rIMGUI.Fonts->Fonts.size());
+
+  pGUI->drop();
+  pDevice->drop();
+
+  CHECK_EQUAL(0,    rIMGUI.Fonts->Fonts.size());
+
+  return;
+}
+
+TEST(TestIMGUIHandle, checkIfHandleSetupsFunctionPointer)
+{
+  ImGuiIO &rIMGUI = ImGui::GetIO();
+
+  rIMGUI.RenderDrawListsFn = nullptr;
+
+  irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
+
+  CHECK_NOT_EQUAL(nullptr, rIMGUI.RenderDrawListsFn);
+
+  pGUI->drop();
+  pDevice->drop();
+
+  return;
+}
+
+TEST(TestIMGUIHandle, checkFrameUpdate)
+{
+  ImGuiIO &rIMGUI = ImGui::GetIO();
+
+  irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
+
+  pDevice->getTimer()->setTime(0);
+
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
+
+  rIMGUI.DeltaTime = 0.0f;
+  rIMGUI.DisplaySize = ImVec2(0.0f, 0.0f);
+  int const LastFrame = ImGui::GetFrameCount();
+
+  pDevice->getTimer()->setTime(16);
+
+  pGUI->startGUI();
+
+  CHECK_NOT_EQUAL(0.0f,    rIMGUI.DisplaySize.x);
+  CHECK_NOT_EQUAL(0.0f,    rIMGUI.DisplaySize.y);
+  CHECK_EQUAL_TOLERANCE(0.016f, 0.001f, ImGui::GetTime());
+  CHECK_EQUAL(LastFrame+1, ImGui::GetFrameCount());
+
+  pGUI->drawAll();
+
+  pDevice->getTimer()->setTime(32);
+
+  pGUI->startGUI();
+
+  CHECK_NOT_EQUAL(0.0f,    rIMGUI.DisplaySize.x);
+  CHECK_NOT_EQUAL(0.0f,    rIMGUI.DisplaySize.y);
+  CHECK_EQUAL_TOLERANCE(0.032f, 0.001f, ImGui::GetTime());
+  CHECK_EQUAL(LastFrame+2, ImGui::GetFrameCount());
+
+  pGUI->drawAll();
+
+  pGUI->drop();
+  pDevice->drop();
+
+  return;
+}
+
+static int ArgumentTest = 42;
+void checkDrawCommand(ImDrawList const * const pParentList, ImDrawCmd const * const pCmd)
+{
+  MOCK_FUNC().withConstPointerParameter("pParentList", pParentList).withConstPointerParameter("pCmd", pCmd);
+
+  POINTERS_EQUAL(&ArgumentTest, pCmd->UserCallbackData);
+
+  return;
+}
+
+TEST(TestIMGUIHandle, checkDrawCallback)
+{
+  ImGuiIO &rIMGUI = ImGui::GetIO();
+
+  irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
+
+  pGUI->startGUI();
+
+  ImGui::Begin("CallbackTest");
+  ImGui::GetWindowDrawList()->AddCallback(checkDrawCommand, static_cast<void *>(&ArgumentTest));
+  ImGui::End();
+
+  mock().expectOneCall("checkDrawCommand").ignoreOtherParameters();
+  mock().ignoreOtherCalls();
+
+  pGUI->drawAll();
+
+  pGUI->drop();
+  pDevice->drop();
+
+  return;
+}
+
+TEST(TestIMGUIHandle, checkFontMethods)
+{
+  ImGuiIO &rIMGUI = ImGui::GetIO();
+
+  irr::IrrlichtDevice * const pDevice = irr::createDevice(irr::video::EDT_NULL);
+  IIMGUIHandle * const pGUI = createIMGUI(pDevice);
+
+  CHECK_EQUAL(1, rIMGUI.Fonts->Fonts.size());
+
+  ImFontConfig FontConfig = *(rIMGUI.Fonts->Fonts[0]->ConfigData);
+  FontConfig.FontDataOwnedByAtlas = false;
+  irr::u8 Data[100];
+
+  // test new font with reused data from default font
+  pGUI->addFont(&FontConfig);
+  pGUI->compileFonts();
+
+  CHECK_EQUAL(2, rIMGUI.Fonts->Fonts.size());
+
+  // test default font creation
+  pGUI->addDefaultFont();
+  pGUI->compileFonts();
+
+  CHECK_EQUAL(3, rIMGUI.Fonts->Fonts.size());
+
+  // test loading from ttf file
+  pGUI->addFontFromFileTTF("../../media/DroidSans.ttf", 16.0f);
+  pGUI->compileFonts();
+
+  CHECK_EQUAL(4, rIMGUI.Fonts->Fonts.size());
+
+  // test loading ttf file from memory (by reusing the default font data again)
+  irr::u8 * const pData = new irr::u8[FontConfig.FontDataSize];
+  for (int i = 0; i < FontConfig.FontDataSize; i++)
+  {
+    pData[i] = (static_cast<irr::u8*>(FontConfig.FontData))[i];
+  }
+  FontConfig.FontDataOwnedByAtlas = false;
+  FontConfig.FontData = nullptr;
+  pGUI->addFontFromMemoryTTF(pData, FontConfig.FontDataSize, FontConfig.SizePixels, &FontConfig);
+  delete[] pData;
+  pGUI->compileFonts();
+
+  CHECK_EQUAL(5, rIMGUI.Fonts->Fonts.size());
+
+  // TODO: We need a compressed and a base85 coded font to test the other two methods.
+  //       If someone has a nice free and small font for that available, please share it...
+
+  // reset all fonts to default
+  pGUI->resetFonts();
+
+  CHECK_EQUAL(1, rIMGUI.Fonts->Fonts.size());
+
+  // also another compile (without another addFont...) must not change the number of stored fonts
+  pGUI->compileFonts();
+
+  CHECK_EQUAL(1, rIMGUI.Fonts->Fonts.size());
+
+  pGUI->drop();
+  pDevice->drop();
+
+  return;
+}
+
+// TODO: check glyph methods
+// TODO: check texture creation methods
+// TODO: check texture update methods
+// TODO: check texture delete method
