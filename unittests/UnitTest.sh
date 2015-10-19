10cd97922e10f2b043c94967dc1daf10151c3f03
Andre Netzeband <andre@netzeband.eu>
2015-10-19 16:58:11 +0200

    Added LD_LIBRARY_PATH setup in bash file.

--- unittests/UnitTest.sh
+++ unittests/UnitTest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-valgrind --tool=memcheck --leak-check=full --track-origins=yes ./UnitTests -v -c
+export LD_LIBRARY_PATH=. && valgrind --tool=memcheck --leak-check=full --track-origins=yes ./UnitTests -v -c
