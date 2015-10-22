f3740e664cdf40d853ffbc0d0123bb732fc22874
Andre Netzeband <andre@netzeband.eu>
2015-10-22 22:35:26 +0200

    Improved travis files.

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -ev
 
+# setup dependency directory
+mkdir travis-dep
+
 # install irrlicht
-wget http://irrimgui.netzeband.eu/travis-dep/irrlicht-1.8.3.zip -O /tmp/irrlicht.zip
-unzip /tmp/irrlicht.zip
-rm /tmp/irrlicht.zip
+wget http://irrimgui.netzeband.eu/travis-dep/irrlicht-1.8.3.zip -O /travis-dep/irrlicht.zip
+unzip /travis-dep/irrlicht.zip
+rm /travis-dep/irrlicht.zip
 
 # install cpputest
-wget http://irrimgui.netzeband.eu/travis-dep/cpputest-3.7.1.zip -O /tmp/cpputest.zip
-unzip /tmp/cpputest.zip
-rm /tmp/cpputest.zip
+wget http://irrimgui.netzeband.eu/travis-dep/cpputest-3.7.1.zip -O /travis-dep/cpputest.zip
+unzip /travis-dep/cpputest.zip
+rm /travis-dep/cpputest.zip
