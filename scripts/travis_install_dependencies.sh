9e797629bd7f294aa3142a2612fe4c9289232391
Andre Netzeband <andre@netzeband.eu>
2015-10-22 23:34:21 +0200

    Improved travis scripts.

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#set -ev
 
 # goto build directory
 cd ${TRAVIS_BUILD_DIR}
 
 # setup dependency directory
 mkdir -p travis-dep
+
+# install imgui
+wget http://irrimgui.netzeband.eu/travis-dep/imgui-1.4.6.zip -O travis-dep/imgui.zip
+unzip travis-dep/imgui.zip -d travis-dep
+rm travis-dep/imgui.zip
+cp -fR travis-dep/imgui-1.4.6/* ${TRAVIS_BUILD_DIR}/dependency/IMGUI
 
 # install irrlicht
 wget http://irrimgui.netzeband.eu/travis-dep/irrlicht-1.8.3.zip -O travis-dep/irrlicht.zip
---
+++ scripts/travis_prepare_cmake.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# goto build directory
+cd ${TRAVIS_BUILD_DIR}
+
+# setup build directory
+mkdir -p travis-build
+cd travis-build
+
+# run cmake
+cmake -version
+cmake \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/travis-install \
+  -DIRRIMGUI_STATIC_LIBRARY=On \
+  -DIRRLICHT_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht-1.8.3/install/gcc-linux64/debug/static/includes/Irrlicht \
+  -DIRRLICHT_LIB_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht-1.8.3/install/gcc-linux64/debug/static/lib/libIrrlicht.a \
+  -DIRRIMGUI_BUILD_UNITTESTS=On \
+  -DIRRIMGUI_CPPUTEST_EXTENSION=${TRAVIS_BUILD_DIR}/travis-dep/cpputest-3.7.1/install/gcc-linux64/debug/static/lib/libCppUTestExt.a \
+  -DIRRIMGUI_CPPUTEST_MAIN=${TRAVIS_BUILD_DIR}/travis-dep/cpputest-3.7.1/install/gcc-linux64/debug/static/lib/libCppUTest.a \
+  -DIRRIMGUI_CPPUTEST_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/cpputest-3.7.1/install/gcc-linux64/debug/static/include \
+  ${TRAVIS_BUILD_DIR}
+
+
