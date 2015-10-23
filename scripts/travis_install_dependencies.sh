e975879057decd3694d4afee1386736eaf2e6280
Andre Netzeband <andre@netzeband.eu>
2015-10-23 22:04:41 +0200

    Enable caching for dependencies.

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -5,19 +5,29 @@ cd ${TRAVIS_BUILD_DIR}
 
 # setup dependency directory
 mkdir -p travis-dep
+mkdir -p ${HOME}/.cache
 
 # install imgui
-wget http://irrimgui.netzeband.eu/travis-dep/imgui-1.4.6.zip -O travis-dep/imgui.zip
+if [ ! -f ${HOME}/.cache/imgui-1.4.6.zip ]; then  
+  wget http://irrimgui.netzeband.eu/travis-dep/imgui-1.4.6.zip -O ${HOME}/.cache/imgui-1.4.6.zip 
+fi
+cp ${HOME}/.cache/imgui-1.4.6.zip travis-dep/imgui.zip
 unzip travis-dep/imgui.zip -d travis-dep
 rm travis-dep/imgui.zip
 cp -fR travis-dep/imgui-1.4.6/* ${TRAVIS_BUILD_DIR}/dependency/IMGUI
 
 # install irrlicht
-wget http://irrimgui.netzeband.eu/travis-dep/irrlicht-1.8.3.zip -O travis-dep/irrlicht.zip
+if [ ! -f ${HOME}/.cache/irrlicht-1.8.3.zip ]; then  
+  wget http://irrimgui.netzeband.eu/travis-dep/irrlicht-1.8.3.zip -O ${HOME}/.cache/irrlicht-1.8.3.zip 
+fi
+cp ${HOME}/.cache/irrlicht-1.8.3.zip travis-dep/irrlicht.zip
 unzip travis-dep/irrlicht.zip -d travis-dep
 rm travis-dep/irrlicht.zip
 
 # install cpputest
-wget http://irrimgui.netzeband.eu/travis-dep/cpputest-3.7.1.zip -O travis-dep/cpputest.zip
+if [ ! -f ${HOME}/.cache/cpputest-3.7.1.zip ]; then  
+  wget http://irrimgui.netzeband.eu/travis-dep/cpputest-3.7.1.zip -O ${HOME}/.cache/cpputest-3.7.1.zip 
+fi
+cp ${HOME}/.cache/cpputest-3.7.1.zip travis-dep/cpputest.zip
 unzip travis-dep/cpputest.zip -d travis-dep
 rm travis-dep/cpputest.zip
