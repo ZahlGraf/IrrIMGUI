688589bc2b0a9f96c515feeb52b769da310c7a1f
Andre Netzeband <andre@netzeband.eu>
2015-10-23 22:45:13 +0200

    Added more verbosity to travis script. Added dependency version numbers to travis.yml

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -4,9 +4,10 @@
 SERVER=http://irrimgui.netzeband.eu/travis-dep
 
 # dependency versions
-IMGUI_VER=imgui-1.4.6
-IRRLICHT_VER=irrlicht-1.8.3
-CPPUTEST_VER=cpputest-3.7.1
+echo "Looking for dependencies:"
+echo " - ${IMGUI_VER}"
+echo " - ${IRRLICHT_VER}"
+echo " - ${CPPUTEST_VER}"
 
 # goto build directory
 cd ${TRAVIS_BUILD_DIR}
@@ -18,29 +19,35 @@ mkdir -p ${HOME}/.cache
 # install imgui
 if [ ! -f ${HOME}/.cache/${IMGUI_VER}.zip ]; then  
   wget ${SERVER}/${IMGUI_VER}.zip -O ${HOME}/.cache/${IMGUI_VER}.zip 
+else
+  echo "Found ${IMGUI_VER}.zip in cache. Skip download..."
 fi
 cp ${HOME}/.cache/${IMGUI_VER}.zip travis-dep/imgui.zip
 unzip travis-dep/imgui.zip -d travis-dep
-rm travis-dep/imgui.zip
-cp -fR travis-dep/${IMGUI_VER}/* ${TRAVIS_BUILD_DIR}/dependency/IMGUI
-rm -R travis-dep/${IMGUI_VER}
+rm -v travis-dep/imgui.zip
+cp -fRv travis-dep/${IMGUI_VER}/* ${TRAVIS_BUILD_DIR}/dependency/IMGUI
+rm -Rv travis-dep/${IMGUI_VER}
 
 # install irrlicht
 if [ ! -f ${HOME}/.cache/${IRRLICHT_VER}.zip ]; then  
   wget ${SERVER}/${IRRLICHT_VER}.zip -O ${HOME}/.cache/${IRRLICHT_VER}.zip 
+else
+  echo "Found ${IMGUI_VER}.zip in cache. Skip download..."
 fi
 cp ${HOME}/.cache/${IRRLICHT_VER}.zip travis-dep/irrlicht.zip
 unzip travis-dep/irrlicht.zip -d travis-dep
-rm travis-dep/irrlicht.zip
-cp -fR travis-dep/${IRRLICHT_VER} travis-dep/irrlicht
-rm -R travis-dep/${IRRLICHT_VER}
+rm -v travis-dep/irrlicht.zip
+cp -fRv travis-dep/${IRRLICHT_VER} travis-dep/irrlicht
+rm -Rv travis-dep/${IRRLICHT_VER}
 
 # install cpputest
 if [ ! -f ${HOME}/.cache/${CPPUTEST_VER}.zip ]; then  
   wget ${SERVER}/${CPPUTEST_VER}.zip -O ${HOME}/.cache/${CPPUTEST_VER}.zip 
+else
+  echo "Found ${IMGUI_VER}.zip in cache. Skip download..."
 fi
 cp ${HOME}/.cache/${CPPUTEST_VER}.zip travis-dep/cpputest.zip
 unzip travis-dep/cpputest.zip -d travis-dep
-rm travis-dep/cpputest.zip
-cp -fR travis-dep/${CPPUTEST_VER} travis-dep/cpputest
-rm -R travis-dep/${CPPUTEST_VER}
+rm -v travis-dep/cpputest.zip
+cp -fRv travis-dep/${CPPUTEST_VER} travis-dep/cpputest
+rm -Rv travis-dep/${CPPUTEST_VER}
