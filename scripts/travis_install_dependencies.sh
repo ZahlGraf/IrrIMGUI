5284ad4634ab2238f7080909e71b0ad1ead99659
Andre Netzeband <andre@netzeband.eu>
2015-10-23 22:36:08 +0200

    Added variables for dependencies.

--- scripts/travis_install_dependencies.sh
+++ scripts/travis_install_dependencies.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# download server
+SERVER=http://irrimgui.netzeband.eu/travis-dep
+
+# dependency versions
+IMGUI_VER=imgui-1.4.6
+IRRLICHT_VER=irrlicht-1.8.3
+CPPUTEST_VER=cpputest-3.7.1
+
 # goto build directory
 cd ${TRAVIS_BUILD_DIR}
 
@@ -8,26 +16,31 @@ mkdir -p travis-dep
 mkdir -p ${HOME}/.cache
 
 # install imgui
-if [ ! -f ${HOME}/.cache/imgui-1.4.6.zip ]; then  
-  wget http://irrimgui.netzeband.eu/travis-dep/imgui-1.4.6.zip -O ${HOME}/.cache/imgui-1.4.6.zip 
+if [ ! -f ${HOME}/.cache/${IMGUI_VER}.zip ]; then  
+  wget ${SERVER}/${IMGUI_VER}.zip -O ${HOME}/.cache/${IMGUI_VER}.zip 
 fi
-cp ${HOME}/.cache/imgui-1.4.6.zip travis-dep/imgui.zip
+cp ${HOME}/.cache/${IMGUI_VER}.zip travis-dep/imgui.zip
 unzip travis-dep/imgui.zip -d travis-dep
 rm travis-dep/imgui.zip
-cp -fR travis-dep/imgui-1.4.6/* ${TRAVIS_BUILD_DIR}/dependency/IMGUI
+cp -fR travis-dep/${IMGUI_VER}/* ${TRAVIS_BUILD_DIR}/dependency/IMGUI
+rm -R travis-dep/${IMGUI_VER}
 
 # install irrlicht
-if [ ! -f ${HOME}/.cache/irrlicht-1.8.3.zip ]; then  
-  wget http://irrimgui.netzeband.eu/travis-dep/irrlicht-1.8.3.zip -O ${HOME}/.cache/irrlicht-1.8.3.zip 
+if [ ! -f ${HOME}/.cache/${IRRLICHT_VER}.zip ]; then  
+  wget ${SERVER}/${IRRLICHT_VER}.zip -O ${HOME}/.cache/${IRRLICHT_VER}.zip 
 fi
-cp ${HOME}/.cache/irrlicht-1.8.3.zip travis-dep/irrlicht.zip
+cp ${HOME}/.cache/${IRRLICHT_VER}.zip travis-dep/irrlicht.zip
 unzip travis-dep/irrlicht.zip -d travis-dep
 rm travis-dep/irrlicht.zip
+cp -fR travis-dep/${IRRLICHT_VER} travis-dep/irrlicht
+rm -R travis-dep/${IRRLICHT_VER}
 
 # install cpputest
-if [ ! -f ${HOME}/.cache/cpputest-3.7.1.zip ]; then  
-  wget http://irrimgui.netzeband.eu/travis-dep/cpputest-3.7.1.zip -O ${HOME}/.cache/cpputest-3.7.1.zip 
+if [ ! -f ${HOME}/.cache/${CPPUTEST_VER}.zip ]; then  
+  wget ${SERVER}/${CPPUTEST_VER}.zip -O ${HOME}/.cache/${CPPUTEST_VER}.zip 
 fi
-cp ${HOME}/.cache/cpputest-3.7.1.zip travis-dep/cpputest.zip
+cp ${HOME}/.cache/${CPPUTEST_VER}.zip travis-dep/cpputest.zip
 unzip travis-dep/cpputest.zip -d travis-dep
 rm travis-dep/cpputest.zip
+cp -fR travis-dep/${CPPUTEST_VER} travis-dep/cpputest
+rm -R travis-dep/${CPPUTEST_VER}
--- scripts/travis_prepare_cmake.sh
+++ scripts/travis_prepare_cmake.sh
@@ -13,12 +13,12 @@ cmake \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/travis-install \
   -DIRRIMGUI_STATIC_LIBRARY=On \
-  -DIRRLICHT_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht-1.8.3/install/gcc-linux64/debug/static/includes/Irrlicht \
-  -DIRRLICHT_LIB_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht-1.8.3/install/gcc-linux64/debug/static/lib/libIrrlicht.a \
+  -DIRRLICHT_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/gcc-linux64/debug/static/includes/Irrlicht \
+  -DIRRLICHT_LIB_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/gcc-linux64/debug/static/lib/libIrrlicht.a \
   -DIRRIMGUI_BUILD_UNITTESTS=On \
-  -DIRRIMGUI_CPPUTEST_EXTENSION=${TRAVIS_BUILD_DIR}/travis-dep/cpputest-3.7.1/install/gcc-linux64/debug/static/lib/libCppUTestExt.a \
-  -DIRRIMGUI_CPPUTEST_MAIN=${TRAVIS_BUILD_DIR}/travis-dep/cpputest-3.7.1/install/gcc-linux64/debug/static/lib/libCppUTest.a \
-  -DIRRIMGUI_CPPUTEST_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/cpputest-3.7.1/install/gcc-linux64/debug/static/include \
+  -DIRRIMGUI_CPPUTEST_EXTENSION=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/gcc-linux64/debug/static/lib/libCppUTestExt.a \
+  -DIRRIMGUI_CPPUTEST_MAIN=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/gcc-linux64/debug/static/lib/libCppUTest.a \
+  -DIRRIMGUI_CPPUTEST_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/gcc-linux64/debug/static/include \
   ${TRAVIS_BUILD_DIR}
 
 
