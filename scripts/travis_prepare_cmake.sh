b5ae8e809eb7f42a8d29235179e621259f03fd99
Andre Netzeband <andre@netzeband.eu>
2015-10-23 23:56:37 +0200

    Make build script more variable as preparation of multi-build.

--- scripts/travis_prepare_cmake.sh
+++ scripts/travis_prepare_cmake.sh
@@ -2,9 +2,16 @@
 
 # print build configuration
 echo "Build configuration:"
+echo " - Compiler:      ${COMPILER}"
 echo " - Library mode:  ${LIBRARY_MODE}"
 echo " - Build Type:    ${BUILD_TYPE}"
 echo " - Native OpenGl: ${NATIVE_OPENGL}"
+
+SMALL_BUILDTYPE=`echo ${BUILD_TYPE} | tr '[:upper:]' '[:lower:]'`
+BUILD_STATIC=Off
+if [ "${LIBRARY_MODE}" == "static" ]; then
+  BUILD_STATIC=On
+fi
 
 # get irrlicht version number
 IRRLICHT_VER_NUMBER=`echo ${IRRLICHT_VER} | sed -s "s/irrlicht-\([0-9.]\+\)/\1/g"`
@@ -20,16 +27,16 @@ cd travis-build
 # run cmake
 cmake -version
 cmake \
-  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
   -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/travis-install \
-  -DIRRIMGUI_STATIC_LIBRARY=On \
-  -DIRRLICHT_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/gcc-linux64/debug/static/includes/Irrlicht \
-  -DIRRLICHT_LIB_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/gcc-linux64/debug/static/lib/libIrrlicht.a \
-  -DIRRLICHT_BIN_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/gcc-linux64/debug/static/lib/libIrrlicht.so.${IRRLICHT_VER_NUMBER} \
+  -DIRRIMGUI_STATIC_LIBRARY=${BUILD_STATIC} \
+  -DIRRLICHT_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/${COMPILER}/${SMALL_BUILDTYPE}/${LIBRARY_MODE}/includes/Irrlicht \
+  -DIRRLICHT_LIB_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/${COMPILER}/${SMALL_BUILDTYPE}/${LIBRARY_MODE}/lib/libIrrlicht.a \
+  -DIRRLICHT_BIN_DIR=${TRAVIS_BUILD_DIR}/travis-dep/irrlicht/install/${COMPILER}/${SMALL_BUILDTYPE}/${LIBRARY_MODE}/lib/libIrrlicht.so.${IRRLICHT_VER_NUMBER} \
   -DIRRIMGUI_BUILD_UNITTESTS=On \
-  -DIRRIMGUI_CPPUTEST_EXTENSION=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/gcc-linux64/debug/static/lib/libCppUTestExt.a \
-  -DIRRIMGUI_CPPUTEST_MAIN=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/gcc-linux64/debug/static/lib/libCppUTest.a \
-  -DIRRIMGUI_CPPUTEST_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/gcc-linux64/debug/static/include \
+  -DIRRIMGUI_CPPUTEST_EXTENSION=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/${COMPILER}/${SMALL_BUILDTYPE}/static/lib/libCppUTestExt.a \
+  -DIRRIMGUI_CPPUTEST_MAIN=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/${COMPILER}/${SMALL_BUILDTYPE}/static/lib/libCppUTest.a \
+  -DIRRIMGUI_CPPUTEST_INCLUDE_DIR=${TRAVIS_BUILD_DIR}/travis-dep/cpputest/install/${COMPILER}/${SMALL_BUILDTYPE}/static/include \
   -DIRRIMGUI_NATIVE_OPENGL=${NATIVE_OPENGL} \
   -DIRRIMGUI_FAST_OPENGL_TEXTURE_CREATION=${NATIVE_OPENGL} \
   ${TRAVIS_BUILD_DIR}
